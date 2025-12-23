e7eea591456c7edf563a019abc5072eca660b026
AyushTidke460 <ayushtidke@gmail.com>
2025-12-23 15:46:29 +0530

    Update streamlit_app.py
    changed title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
-st.title(f"Customize Your Smoothie! :cup_with_straw:")
+st.title(f"My Parents New Healthy Diner")
 st.write(
   """ Choose the fruits you want in your custom Smoothie!
   """
